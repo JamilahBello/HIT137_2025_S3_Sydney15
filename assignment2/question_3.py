dfdb241ac74db53066ab33dc431fd0ac93fa0d46
codevibek <vibek2925@gmail.com>
2026-01-14 11:18:48 +1100

    error handling

--- assignment2/question_3.py
+++ assignment2/question_3.py
@@ -1,30 +1,52 @@
 # starting out
 import turtle
+
 draw = turtle.Turtle()
 
-no_of_sides = int(input("Enter number of sides : "))
-side_length = 10
-# input("Enter side length : ")
-recursion_depth = int(input("Enter recursion depth : "))
+while True:
+    sides = input("Enter number of sides : ")
+    if not sides.isdigit():  # https://realpython.com/python-not-operator/
+        print("Please enter a  number")
+        continue
+    no_of_sides = int(sides)
+    if no_of_sides >= 3:
+        break
+    else:
+        print("You need atleast 3 sides for a polygon. Enter valid number.")
+        continue
+
+while True:
+    length = input("Enter side length : ")
+    if not length.isdigit():
+        print("Please enter a number")
+        continue
+    side_length = int(length)
+    break
+
+while True:
+    depth = input("Enter recursion depth : ")
+    if not depth.isdigit():
+        print("Please enter a number")
+        continue
+    recursion_depth = int(depth)
+    break
+
 
 def recuring_shape(depth):
-    if depth == 0 :
+    if depth == 0:
         draw.forward(side_length)
     else:
-        recuring_shape(depth-1)
+        recuring_shape(depth - 1)
         draw.right(60)
-        recuring_shape(depth-1)
+        recuring_shape(depth - 1)
         draw.left(120)
-        recuring_shape(depth-1)
+        recuring_shape(depth - 1)
         draw.right(60)
-        recuring_shape(depth-1)
+        recuring_shape(depth - 1)
 
 
 for side_num in range(int(no_of_sides)):
     recuring_shape(recursion_depth)
-    draw.right(90)
-
-
-
+    draw.right(360 / no_of_sides)
 
 turtle.done()
